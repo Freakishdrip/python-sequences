bfd3e21df728efad77e46e9943159a3e033db9c5
Nick Drummond <nckdrmmnd@gmail.com>
2024-01-30 12:46:39 -0700

    sometimes less work is more work

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+fibonacci_list = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 
 def print_fibonacci(length):
-    pass
+    arr = []
+    i = 0
+    print(f"{fibonacci_list[:length]}")
+    # while(i <= length):
+    #   arr.append(fibonacci_list[i])
+    #   # print(fibonacci_list[i])
+    #   i += 1
+    # print(arr)
+print_fibonacci(0)
